3e53ee119c740197eedcd0fbe37eb1337771e643
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2025-04-18 17:02:35 +0530

    Initial commit with hello api as test

---
+++ src/test/java/com/crazy_nuclei/TodoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazy_nuclei.TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
